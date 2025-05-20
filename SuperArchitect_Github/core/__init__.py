f1652e5e20be0a0aa65b866604d1651288cf3735
Okkay914 <williambaumgarth@Gmail.com>
2025-05-20 17:56:49 -0400

    Update __init__.py

--- SuperArchitect_Github/core/__init__.py
+++ SuperArchitect_Github/core/__init__.py
@@ -2,4 +2,3 @@
 from .query_manager import QueryManager
 from .analysis import AnalyzerEngine # Add this
 from .synthesis import SynthesisEngine # Add this
-from .file_manager import FileManager # Add this
